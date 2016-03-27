dc21be3e49bbada9f1919885b7a99a4815fff32e
dkarkada <dkarkada@gmail.com>
2016-03-27 15:14:53 -0500

    Created

---
+++ SquareLattice2D-V1/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace SquareLattice2D_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.fwdButton = new System.Windows.Forms.Button();
+            this.xProbBar = new System.Windows.Forms.TrackBar();
+            this.stepLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.speedBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorChooser = new System.Windows.Forms.GroupBox();
+            this.pinkRadioButton = new System.Windows.Forms.RadioButton();
+            this.whiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.blueRadioButton = new System.Windows.Forms.RadioButton();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.xProbBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.colorChooser.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Enabled = false;
+            this.backButton.Location = new System.Drawing.Point(49, 110);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(40, 40);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "<";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Visible = false;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(96, 110);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(69, 40);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // fwdButton
+            // 
+            this.fwdButton.Location = new System.Drawing.Point(171, 110);
+            this.fwdButton.Name = "fwdButton";
+            this.fwdButton.Size = new System.Drawing.Size(40, 40);
+            this.fwdButton.TabIndex = 2;
+            this.fwdButton.Text = ">";
+            this.fwdButton.UseVisualStyleBackColor = true;
+            this.fwdButton.Click += new System.EventHandler(this.fwdButton_Click);
+            // 
+            // xProbBar
+            // 
+            this.xProbBar.Location = new System.Drawing.Point(16, 9);
+            this.xProbBar.Maximum = 100;
+            this.xProbBar.Name = "xProbBar";
+            this.xProbBar.Size = new System.Drawing.Size(231, 45);
+            this.xProbBar.TabIndex = 3;
+            this.xProbBar.TickFrequency = 10;
+            this.xProbBar.Value = 100;
+            this.xProbBar.Scroll += new System.EventHandler(this.xProbBar_Scroll);
+            // 
+            // stepLabel
+            // 
+            this.stepLabel.AutoSize = true;
+            this.stepLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.stepLabel.Location = new System.Drawing.Point(217, 122);
+            this.stepLabel.Name = "stepLabel";
+            this.stepLabel.Size = new System.Drawing.Size(16, 17);
+            this.stepLabel.TabIndex = 4;
+            this.stepLabel.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Play Speed";
+            this.label1.Visible = false;
+            // 
+            // speedBar
+            // 
+            this.speedBar.Location = new System.Drawing.Point(16, 168);
+            this.speedBar.Maximum = 100;
+            this.speedBar.Name = "speedBar";
+            this.speedBar.Size = new System.Drawing.Size(231, 45);
+            this.speedBar.TabIndex = 5;
+            this.speedBar.TickFrequency = 10;
+            this.speedBar.Value = 50;
+            this.speedBar.Visible = false;
+            this.speedBar.Scroll += new System.EventHandler(this.speedBar_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "X and Y probability";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 97.0696F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.930403F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(270, 761);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(262, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(8, 761);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.colorChooser);
+            this.panel1.Controls.Add(this.restartButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.backButton);
+            this.panel1.Controls.Add(this.xProbBar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.playButton);
+            this.panel1.Controls.Add(this.stepLabel);
+            this.panel1.Controls.Add(this.fwdButton);
+            this.panel1.Controls.Add(this.speedBar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 761);
+            this.panel1.TabIndex = 8;
+            // 
+            // colorChooser
+            // 
+            this.colorChooser.Controls.Add(this.pinkRadioButton);
+            this.colorChooser.Controls.Add(this.whiteRadioButton);
+            this.colorChooser.Controls.Add(this.blueRadioButton);
+            this.colorChooser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorChooser.Location = new System.Drawing.Point(64, 340);
+            this.colorChooser.Name = "colorChooser";
+            this.colorChooser.Size = new System.Drawing.Size(140, 108);
+            this.colorChooser.TabIndex = 0;
+            this.colorChooser.TabStop = false;
+            this.colorChooser.Text = "Drawing Color";
+            // 
+            // pinkRadioButton
+            // 
+            this.pinkRadioButton.AutoSize = true;
+            this.pinkRadioButton.Location = new System.Drawing.Point(23, 74);
+            this.pinkRadioButton.Name = "pinkRadioButton";
+            this.pinkRadioButton.Size = new System.Drawing.Size(90, 21);
+            this.pinkRadioButton.TabIndex = 2;
+            this.pinkRadioButton.TabStop = true;
+            this.pinkRadioButton.Text = "Rose Pink";
+            this.pinkRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // whiteRadioButton
+            // 
+            this.whiteRadioButton.AutoSize = true;
+            this.whiteRadioButton.Location = new System.Drawing.Point(23, 46);
+            this.whiteRadioButton.Name = "whiteRadioButton";
+            this.whiteRadioButton.Size = new System.Drawing.Size(96, 21);
+            this.whiteRadioButton.TabIndex = 1;
+            this.whiteRadioButton.TabStop = true;
+            this.whiteRadioButton.Text = "Ivory White";
+            this.whiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // blueRadioButton
+            // 
+            this.blueRadioButton.AutoSize = true;
+            this.blueRadioButton.Checked = true;
+            this.blueRadioButton.Location = new System.Drawing.Point(23, 19);
+            this.blueRadioButton.Name = "blueRadioButton";
+            this.blueRadioButton.Size = new System.Drawing.Size(86, 21);
+            this.blueRadioButton.TabIndex = 0;
+            this.blueRadioButton.TabStop = true;
+            this.blueRadioButton.Text = "Pale Blue";
+            this.blueRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // restartButton
+            // 
+            this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartButton.Location = new System.Drawing.Point(96, 256);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(69, 36);
+            this.restartButton.TabIndex = 7;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(270, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1154, 761);
+            this.panel3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1424, 761);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Square Lattice 2D";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.xProbBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.colorChooser.ResumeLayout(false);
+            this.colorChooser.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button fwdButton;
+        private System.Windows.Forms.TrackBar xProbBar;
+        private System.Windows.Forms.Label stepLabel;
+        private System.Windows.Forms.TrackBar speedBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.GroupBox colorChooser;
+        private System.Windows.Forms.RadioButton whiteRadioButton;
+        private System.Windows.Forms.RadioButton blueRadioButton;
+        private System.Windows.Forms.RadioButton pinkRadioButton;
+    }
+}
+
---
+++ SquareLattice2D-V1/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SquareLattice2D_V1
+{
+    public partial class Form1 : Form
+    {
+        Canvas c;
+        Bitmap image;
+        Lattice lat;
+        List<LatticeChange> latChanges;
+        int step = 1;
+        Random rnd = new Random();
+        GraphicsSettings gs;
+        System.Timers.Timer clock;
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new Form1());
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            gs = new GraphicsSettings();
+            Size = Screen.FromControl(this).WorkingArea.Size;
+            image = new Bitmap(panel3.Width, panel3.Height);
+            SetControls();
+
+            lat = new Lattice();
+            latChanges = new List<LatticeChange>();
+            clock = new System.Timers.Timer(500);
+            clock.AutoReset = true;
+            clock.Elapsed += new System.Timers.ElapsedEventHandler(PlayStep);
+            
+
+            foreach (Atom a in lat.mat)
+            {
+                if (a.x == 0 && a.y == 0)
+                {
+                    LatticeChange lc = new LatticeChange();
+                    lc.AddOn(a);
+                    latChanges.Add(lc);
+                    a.excited = true;
+                }
+            }
+            DoubleBuffered = true;
+            PaintImage(true);
+        }
+        private void SetControls()
+        {
+            c = new Canvas(image);
+            c.Dock = DockStyle.Fill;
+            panel3.Controls.Add(c);
+
+            BackColor = gs.bgBrush.Color;
+            panel1.BackColor = Color.FromArgb(80, 80, 80);
+            colorChooser.ForeColor = panel2.BackColor = label1.ForeColor = label2.ForeColor = stepLabel.ForeColor = Color.White;
+
+            Resize += new EventHandler(Resized);
+            System.EventHandler eh = new System.EventHandler(ColorChanged);
+            blueRadioButton.CheckedChanged += eh;
+            whiteRadioButton.CheckedChanged += eh;
+            pinkRadioButton.CheckedChanged += eh;
+        }
+        private void Resized(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+        private void PaintImage(Boolean fwd)
+        {
+            Graphics g = Graphics.FromImage(image);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            if (gs.axes)
+            {
+                g.DrawLine(gs.axisPen, c.Width / 2, 0, c.Width / 2, c.Height);
+                g.DrawLine(gs.axisPen, 0, c.Height / 2, c.Width, c.Height / 2);
+            }
+            if (latChanges.Count > 0)
+            {
+                LatticeChange lc = latChanges[latChanges.Count - 1];
+                int sz = lat.aSize;
+                SolidBrush br = gs.fgBrush;
+                if (!fwd)
+                {
+                    br = gs.bgBrush;
+                    latChanges.Remove(lc);
+                    foreach (Atom a in lc.on)
+                    {
+                        a.excited = false;
+                    }
+                }
+                foreach (Atom a in lc.on)
+                {
+                    g.FillRectangle(br, GetCanvXPos(a.x) - sz / 2, GetCanvYPos(a.y) - sz / 2, sz, sz);
+                }
+            }
+            c.Invalidate();
+            Refresh();
+        }
+        private void Redraw()
+        {
+            Graphics g = Graphics.FromImage(image);
+            g.Clear(Color.Transparent);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            if (gs.axes)
+            {
+                g.DrawLine(gs.axisPen, c.Width / 2, 0, c.Width / 2, c.Height);
+                g.DrawLine(gs.axisPen, 0, c.Height / 2, c.Width, c.Height / 2);
+            }
+            foreach (LatticeChange lc in latChanges)
+            {
+                int sz = lat.aSize;
+                SolidBrush br = gs.fgBrush;
+                foreach (Atom a in lc.on)
+                {
+                    g.FillRectangle(br, GetCanvXPos(a.x) - sz / 2, GetCanvYPos(a.y) - sz / 2, sz, sz);
+                }
+            }
+            c.Invalidate();
+            Refresh();
+        }
+        private void Step()
+        {
+            step++;
+            LatticeChange lc = new LatticeChange();
+            for (int r = 1; r < lat.mat.GetLength(0) - 1; r++)
+            {
+                for (int c = 1; c < lat.mat.GetLength(1) - 1; c++)
+                {
+                    Atom a = lat.mat[r, c];
+                    if (!a.excited)
+                    {
+                        int numActiveNeighbors = 0;
+                        if (lat.mat[r + 1, c].excited) numActiveNeighbors++;
+                        if (lat.mat[r, c + 1].excited) numActiveNeighbors++;
+                        if (lat.mat[r - 1, c].excited) numActiveNeighbors++;
+                        if (lat.mat[r, c - 1].excited) numActiveNeighbors++;
+                        if (numActiveNeighbors == 1 && rnd.NextDouble() < lat.probability)
+                            lc.AddOn(a);
+                    }
+                }
+            }
+            stepLabel.Text = "" + step;
+            foreach (Atom a in lc.on)
+                a.excited = true;
+            latChanges.Add(lc);
+            PaintImage(true);
+        }
+        private void PlayStep(object source, System.Timers.ElapsedEventArgs e)
+        {
+            BeginInvoke(new Action(Step), null);
+        }
+        private void StepBack()
+        {
+            if (step > 1) step--;
+            stepLabel.Text = "" + step;
+            PaintImage(false);
+        }
+        private int GetCanvXPos(int x)
+        {
+            return c.Width / 2 + lat.aSize * x;
+        }
+        private int GetCanvYPos(int y)
+        {
+            return c.ClientSize.Height / 2 - lat.aSize * y;
+        }
+        private void ColorChanged(object sender, EventArgs e)
+        {
+            Color c = Color.PaleTurquoise;
+            if (whiteRadioButton.Checked) c = Color.Ivory;
+            if (pinkRadioButton.Checked) c = Color.Pink;
+            gs.fgBrush.Color = gs.axisPen.Color = c;
+            Redraw();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            StepBack();
+        }
+        private void fwdButton_Click(object sender, EventArgs e)
+        {
+            Step();
+        }
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (!clock.Enabled)
+            {
+                clock.Start();
+                playButton.Text = "Pause";
+                speedBar.Visible = label1.Visible = true;
+            }
+            else {
+                clock.Stop();
+                playButton.Text = "Play";
+                speedBar.Visible = label1.Visible = false;
+            }
+        }
+        private void xProbBar_Scroll(object sender, EventArgs e)
+        {
+            lat.probability = (double)(xProbBar.Value) / 100;
+        }
+        private void speedBar_Scroll(object sender, EventArgs e)
+        {
+            clock.Interval = 900 - (speedBar.Value * 8);
+        }
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            step = 1;
+            clock.Stop();
+            lat = new Lattice();
+            latChanges = new List<LatticeChange>();
+            foreach (Atom a in lat.mat)
+            {
+                if (a.x == 0 && a.y == 0)
+                {
+                    LatticeChange lc = new LatticeChange();
+                    lc.AddOn(a);
+                    latChanges.Add(lc);
+                    a.excited = true;
+                }
+            }
+            stepLabel.Text = "1";
+            Redraw();
+        }
+
+    }
+    public class Canvas : Panel
+    {
+        Bitmap image;
+
+        public Canvas(Bitmap b)
+        {
+            image = b;
+            AutoSize = true;
+            DoubleBuffered = true;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(image, 0, 0);
+        }
+    }
+    public class Lattice
+    {
+        public Atom[,] mat { get; set; }
+        public double probability { get; set; }
+        public int aSize { get; set; }
+
+
+        public Lattice()
+        {
+            probability = 1;
+            aSize = 10;
+            mat = new Atom[256, 256];
+            int xsz = mat.GetLength(1);
+            int ysz = mat.GetLength(0);
+            for (int i = 0; i < xsz; i++)
+            {
+                for (int j = 0; j < ysz; j++)
+                {
+                    mat[j, i] = new Atom(i - xsz / 2, j - ysz / 2, 0);
+                }
+            }
+        }
+    }
+    public class Atom
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+        public Boolean excited { get; set; }
+
+        public Atom(int x1, int y1, int z1)
+        {
+            x = x1;
+            y = y1;
+            z = z1;
+            excited = false;
+        }
+    }
+    public class LatticeChange
+    {
+        public List<Atom> on { get; set; }
+        public List<Atom> off { get; set; }
+
+        public LatticeChange()
+        {
+            on = new List<Atom>();
+            off = new List<Atom>();
+        }
+        public void AddOn(Atom a)
+        {
+            on.Add(a);
+        }
+        public void AddOff(Atom a)
+        {
+            off.Add(a);
+        }
+    }
+    public class GraphicsSettings
+    {
+        public SolidBrush bgBrush { get; set; }
+        public SolidBrush fgBrush { get; set; }
+        public Pen axisPen { get; set; }
+        public Boolean axes { get; set; }
+
+        public GraphicsSettings()
+        {
+            bgBrush = new SolidBrush(Color.Black);
+            fgBrush = new SolidBrush(Color.PaleTurquoise);
+            axisPen = new Pen(Color.PaleTurquoise, 1);
+            axes = true;
+        }
+    }
+}
