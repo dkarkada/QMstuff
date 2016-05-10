0f13f56c0203c83e5d2ae47066272c5d6e80ba53
dkarkada <dkarkada@gmail.com>
2016-05-09 19:05:08 -0500

    v2 created i think? Lots of work done

---
+++ QMStuff v2/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace QMStuff_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.stepCounter = new System.Windows.Forms.NumericUpDown();
+			this.yProbValue = new System.Windows.Forms.NumericUpDown();
+			this.xProbValue = new System.Windows.Forms.NumericUpDown();
+			this.yProbLabel = new System.Windows.Forms.Label();
+			this.xProbLabel = new System.Windows.Forms.Label();
+			this.xProbBar = new System.Windows.Forms.TrackBar();
+			this.restartButton = new System.Windows.Forms.Button();
+			this.backButton = new System.Windows.Forms.Button();
+			this.yProbBar = new System.Windows.Forms.TrackBar();
+			this.playSpeedLabel = new System.Windows.Forms.Label();
+			this.playButton = new System.Windows.Forms.Button();
+			this.fwdButton = new System.Windows.Forms.Button();
+			this.speedBar = new System.Windows.Forms.TrackBar();
+			this.BottomToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+			this.TopToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+			this.RightToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+			this.LeftToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+			this.ContentPanel = new System.Windows.Forms.ToolStripContentPanel();
+			this.probGroup = new System.Windows.Forms.Panel();
+			this.playGroup = new System.Windows.Forms.Panel();
+			this.splitPanel = new System.Windows.Forms.SplitContainer();
+			this.FlowLayout = new System.Windows.Forms.FlowLayoutPanel();
+			((System.ComponentModel.ISupportInitialize)(this.stepCounter)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.yProbValue)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.xProbValue)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.xProbBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.yProbBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.speedBar)).BeginInit();
+			this.probGroup.SuspendLayout();
+			this.playGroup.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitPanel)).BeginInit();
+			this.splitPanel.Panel1.SuspendLayout();
+			this.splitPanel.SuspendLayout();
+			this.FlowLayout.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// stepCounter
+			// 
+			this.stepCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.stepCounter.Location = new System.Drawing.Point(266, 27);
+			this.stepCounter.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.stepCounter.Name = "stepCounter";
+			this.stepCounter.Size = new System.Drawing.Size(53, 23);
+			this.stepCounter.TabIndex = 14;
+			this.stepCounter.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// yProbValue
+			// 
+			this.yProbValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.yProbValue.Location = new System.Drawing.Point(19, 81);
+			this.yProbValue.Name = "yProbValue";
+			this.yProbValue.Size = new System.Drawing.Size(49, 23);
+			this.yProbValue.TabIndex = 12;
+			this.yProbValue.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			this.yProbValue.ValueChanged += new System.EventHandler(this.yProbValue_ValueChanged);
+			// 
+			// xProbValue
+			// 
+			this.xProbValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.xProbValue.Location = new System.Drawing.Point(19, 26);
+			this.xProbValue.Name = "xProbValue";
+			this.xProbValue.Size = new System.Drawing.Size(49, 23);
+			this.xProbValue.TabIndex = 11;
+			this.xProbValue.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			this.xProbValue.ValueChanged += new System.EventHandler(this.xProbValue_ValueChanged);
+			// 
+			// yProbLabel
+			// 
+			this.yProbLabel.AutoSize = true;
+			this.yProbLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.yProbLabel.Location = new System.Drawing.Point(336, 83);
+			this.yProbLabel.Name = "yProbLabel";
+			this.yProbLabel.Size = new System.Drawing.Size(49, 17);
+			this.yProbLabel.TabIndex = 10;
+			this.yProbLabel.Text = "y Prob";
+			// 
+			// xProbLabel
+			// 
+			this.xProbLabel.AutoSize = true;
+			this.xProbLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.xProbLabel.Location = new System.Drawing.Point(336, 27);
+			this.xProbLabel.Name = "xProbLabel";
+			this.xProbLabel.Size = new System.Drawing.Size(48, 17);
+			this.xProbLabel.TabIndex = 9;
+			this.xProbLabel.Text = "x Prob";
+			// 
+			// xProbBar
+			// 
+			this.xProbBar.Location = new System.Drawing.Point(80, 27);
+			this.xProbBar.Maximum = 100;
+			this.xProbBar.Name = "xProbBar";
+			this.xProbBar.Size = new System.Drawing.Size(250, 45);
+			this.xProbBar.TabIndex = 8;
+			this.xProbBar.TickFrequency = 10;
+			this.xProbBar.Value = 100;
+			this.xProbBar.Scroll += new System.EventHandler(this.xProbBar_Scroll);
+			// 
+			// restartButton
+			// 
+			this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.restartButton.Location = new System.Drawing.Point(257, 74);
+			this.restartButton.Name = "restartButton";
+			this.restartButton.Size = new System.Drawing.Size(69, 36);
+			this.restartButton.TabIndex = 7;
+			this.restartButton.Text = "Restart";
+			this.restartButton.UseVisualStyleBackColor = true;
+			this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+			// 
+			// backButton
+			// 
+			this.backButton.Enabled = false;
+			this.backButton.Location = new System.Drawing.Point(42, 17);
+			this.backButton.Name = "backButton";
+			this.backButton.Size = new System.Drawing.Size(40, 40);
+			this.backButton.TabIndex = 0;
+			this.backButton.Text = "<";
+			this.backButton.UseVisualStyleBackColor = true;
+			this.backButton.Click += new System.EventHandler(this.backButton_Click);
+			// 
+			// yProbBar
+			// 
+			this.yProbBar.Location = new System.Drawing.Point(80, 83);
+			this.yProbBar.Maximum = 100;
+			this.yProbBar.Name = "yProbBar";
+			this.yProbBar.Size = new System.Drawing.Size(250, 45);
+			this.yProbBar.TabIndex = 3;
+			this.yProbBar.TickFrequency = 10;
+			this.yProbBar.Value = 100;
+			this.yProbBar.Scroll += new System.EventHandler(this.yProbBar_Scroll);
+			// 
+			// playSpeedLabel
+			// 
+			this.playSpeedLabel.AutoSize = true;
+			this.playSpeedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.playSpeedLabel.Location = new System.Drawing.Point(81, 107);
+			this.playSpeedLabel.Name = "playSpeedLabel";
+			this.playSpeedLabel.Size = new System.Drawing.Size(80, 17);
+			this.playSpeedLabel.TabIndex = 6;
+			this.playSpeedLabel.Text = "Play Speed";
+			this.playSpeedLabel.Visible = false;
+			// 
+			// playButton
+			// 
+			this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.playButton.Location = new System.Drawing.Point(89, 17);
+			this.playButton.Name = "playButton";
+			this.playButton.Size = new System.Drawing.Size(69, 40);
+			this.playButton.TabIndex = 1;
+			this.playButton.Text = "Play";
+			this.playButton.UseVisualStyleBackColor = true;
+			this.playButton.Click += new System.EventHandler(this.playButton_Click);
+			// 
+			// fwdButton
+			// 
+			this.fwdButton.Location = new System.Drawing.Point(164, 17);
+			this.fwdButton.Name = "fwdButton";
+			this.fwdButton.Size = new System.Drawing.Size(40, 40);
+			this.fwdButton.TabIndex = 2;
+			this.fwdButton.Text = ">";
+			this.fwdButton.UseVisualStyleBackColor = true;
+			this.fwdButton.Click += new System.EventHandler(this.fwdButton_Click);
+			// 
+			// speedBar
+			// 
+			this.speedBar.Location = new System.Drawing.Point(8, 80);
+			this.speedBar.Maximum = 100;
+			this.speedBar.Name = "speedBar";
+			this.speedBar.Size = new System.Drawing.Size(231, 45);
+			this.speedBar.TabIndex = 5;
+			this.speedBar.TickFrequency = 10;
+			this.speedBar.Value = 50;
+			this.speedBar.Visible = false;
+			this.speedBar.Scroll += new System.EventHandler(this.speedBar_Scroll);
+			// 
+			// BottomToolStripPanel
+			// 
+			this.BottomToolStripPanel.Location = new System.Drawing.Point(0, 0);
+			this.BottomToolStripPanel.Name = "BottomToolStripPanel";
+			this.BottomToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.BottomToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.BottomToolStripPanel.Size = new System.Drawing.Size(0, 0);
+			// 
+			// TopToolStripPanel
+			// 
+			this.TopToolStripPanel.Location = new System.Drawing.Point(0, 0);
+			this.TopToolStripPanel.Name = "TopToolStripPanel";
+			this.TopToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.TopToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.TopToolStripPanel.Size = new System.Drawing.Size(0, 0);
+			// 
+			// RightToolStripPanel
+			// 
+			this.RightToolStripPanel.Location = new System.Drawing.Point(0, 0);
+			this.RightToolStripPanel.Name = "RightToolStripPanel";
+			this.RightToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.RightToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.RightToolStripPanel.Size = new System.Drawing.Size(0, 0);
+			// 
+			// LeftToolStripPanel
+			// 
+			this.LeftToolStripPanel.Location = new System.Drawing.Point(0, 0);
+			this.LeftToolStripPanel.Name = "LeftToolStripPanel";
+			this.LeftToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.LeftToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.LeftToolStripPanel.Size = new System.Drawing.Size(0, 0);
+			// 
+			// ContentPanel
+			// 
+			this.ContentPanel.Size = new System.Drawing.Size(150, 150);
+			// 
+			// probGroup
+			// 
+			this.probGroup.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.probGroup.Controls.Add(this.yProbLabel);
+			this.probGroup.Controls.Add(this.yProbBar);
+			this.probGroup.Controls.Add(this.yProbValue);
+			this.probGroup.Controls.Add(this.xProbBar);
+			this.probGroup.Controls.Add(this.xProbValue);
+			this.probGroup.Controls.Add(this.xProbLabel);
+			this.probGroup.Location = new System.Drawing.Point(20, 20);
+			this.probGroup.Margin = new System.Windows.Forms.Padding(20);
+			this.probGroup.Name = "probGroup";
+			this.probGroup.Size = new System.Drawing.Size(399, 137);
+			this.probGroup.TabIndex = 0;
+			// 
+			// playGroup
+			// 
+			this.playGroup.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.playGroup.Controls.Add(this.playSpeedLabel);
+			this.playGroup.Controls.Add(this.speedBar);
+			this.playGroup.Controls.Add(this.stepCounter);
+			this.playGroup.Controls.Add(this.fwdButton);
+			this.playGroup.Controls.Add(this.restartButton);
+			this.playGroup.Controls.Add(this.playButton);
+			this.playGroup.Controls.Add(this.backButton);
+			this.playGroup.Location = new System.Drawing.Point(459, 20);
+			this.playGroup.Margin = new System.Windows.Forms.Padding(20);
+			this.playGroup.Name = "playGroup";
+			this.playGroup.Size = new System.Drawing.Size(339, 137);
+			this.playGroup.TabIndex = 15;
+			// 
+			// splitPanel
+			// 
+			this.splitPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitPanel.Location = new System.Drawing.Point(0, 0);
+			this.splitPanel.Name = "splitPanel";
+			// 
+			// splitPanel.Panel1
+			// 
+			this.splitPanel.Panel1.Controls.Add(this.FlowLayout);
+			this.splitPanel.Size = new System.Drawing.Size(1424, 761);
+			this.splitPanel.SplitterDistance = 840;
+			this.splitPanel.TabIndex = 10;
+			// 
+			// FlowLayout
+			// 
+			this.FlowLayout.AutoScroll = true;
+			this.FlowLayout.Controls.Add(this.probGroup);
+			this.FlowLayout.Controls.Add(this.playGroup);
+			this.FlowLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.FlowLayout.Location = new System.Drawing.Point(0, 0);
+			this.FlowLayout.Name = "FlowLayout";
+			this.FlowLayout.Size = new System.Drawing.Size(840, 761);
+			this.FlowLayout.TabIndex = 16;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1424, 761);
+			this.Controls.Add(this.splitPanel);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Square Lattice 2D v2";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			((System.ComponentModel.ISupportInitialize)(this.stepCounter)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.yProbValue)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.xProbValue)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.xProbBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.yProbBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.speedBar)).EndInit();
+			this.probGroup.ResumeLayout(false);
+			this.probGroup.PerformLayout();
+			this.playGroup.ResumeLayout(false);
+			this.playGroup.PerformLayout();
+			this.splitPanel.Panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitPanel)).EndInit();
+			this.splitPanel.ResumeLayout(false);
+			this.FlowLayout.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.TrackBar yProbBar;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button fwdButton;
+        private System.Windows.Forms.TrackBar speedBar;
+        private System.Windows.Forms.TrackBar xProbBar;
+        private System.Windows.Forms.Label playSpeedLabel;
+        private System.Windows.Forms.Label xProbLabel;
+        private System.Windows.Forms.Label yProbLabel;
+		private System.Windows.Forms.NumericUpDown yProbValue;
+		private System.Windows.Forms.NumericUpDown xProbValue;
+		private System.Windows.Forms.NumericUpDown stepCounter;
+		private System.Windows.Forms.ToolStripPanel BottomToolStripPanel;
+		private System.Windows.Forms.ToolStripPanel TopToolStripPanel;
+		private System.Windows.Forms.ToolStripPanel RightToolStripPanel;
+		private System.Windows.Forms.ToolStripPanel LeftToolStripPanel;
+		private System.Windows.Forms.ToolStripContentPanel ContentPanel;
+		private System.Windows.Forms.Panel playGroup;
+		private System.Windows.Forms.Panel probGroup;
+		private System.Windows.Forms.SplitContainer splitPanel;
+		private System.Windows.Forms.FlowLayoutPanel FlowLayout;
+	}
+}
+
---
+++ QMStuff v2/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QMStuff_v2
+{
+	enum direction { N, E, S, W };
+
+	public partial class Form1 : Form {
+		Canvas c;
+		Lattice lat;
+		List<LatticeChange> latChanges;
+		int step = 1;
+		Random rnd = new Random();
+		GraphicsSettings gs;
+		System.Timers.Timer clock;
+
+		public Form1() {
+			InitializeComponent();
+			gs = new GraphicsSettings();
+			SetSizeAndControls();
+
+			lat = new Lattice();
+			latChanges = new List<LatticeChange>();
+			clock = new System.Timers.Timer(500);
+			clock.AutoReset = true;
+			clock.Elapsed += new System.Timers.ElapsedEventHandler(PlayStep);
+
+
+			foreach(Atom a in lat.mat) {
+				if(a.x == 0 && a.y == 0) {
+					LatticeChange lc = new LatticeChange();
+					lc.AddOn(a);
+					latChanges.Add(lc);
+					a.excited = true;
+				}
+			}
+			DoubleBuffered = true;
+			PaintImage(true);
+		}
+		private void SetSizeAndControls() {
+			Size = Screen.FromControl(this).WorkingArea.Size;
+
+			splitPanel.SplitterDistance = ClientSize.Width - splitPanel.Panel2.Height - splitPanel.SplitterWidth;
+
+			c = new Canvas(new Bitmap(splitPanel.Panel2.Width, splitPanel.Panel2.Height));
+			c.Dock = DockStyle.Fill;
+			splitPanel.Panel2.Controls.Add(c);
+
+			splitPanel.Panel2.BackColor = gs.bgBrush.Color;
+			splitPanel.Panel1.BackColor = Color.FromArgb(60, 60, 75);
+			playSpeedLabel.ForeColor = xProbLabel.ForeColor = yProbLabel.ForeColor
+				= Color.White;
+
+			Resize += FrameResizing;
+			splitPanel.SplitterMoved += SplitPanelResized;
+		}
+		private void FrameResizing(object sender, EventArgs e) {
+			splitPanel.SplitterDistance = Math.Max(0, ClientSize.Width - splitPanel.Panel2.Height - splitPanel.SplitterWidth);
+			c.image = new Bitmap(splitPanel.Panel2.Width, splitPanel.Panel2.Height);
+			Redraw();
+		}
+		private void SplitPanelResized(object sender, SplitterEventArgs e) {
+			c.image = new Bitmap(splitPanel.Panel2.Width, splitPanel.Panel2.Height);
+			Redraw();
+		}
+		private void PaintImage(Boolean fwd) {
+			Graphics g = Graphics.FromImage(c.image);
+			g.SmoothingMode = SmoothingMode.AntiAlias;
+			if(gs.axes) {
+				int w = splitPanel.Panel2.Width;
+				int h = splitPanel.Panel2.Height;
+				g.DrawLine(gs.axisPen, w / 2, 0, w / 2, h);
+				g.DrawLine(gs.axisPen, 0, h / 2, w, h / 2);
+			}
+			if(latChanges.Count > 0) {
+				LatticeChange lc = latChanges[latChanges.Count - 1];
+				int sz = lat.aSize;
+				SolidBrush br = gs.fgBrush;
+				if(!fwd) {
+					br = gs.bgBrush;
+					latChanges.Remove(lc);
+					foreach(Atom a in lc.on) {
+						a.excited = false;
+					}
+				}
+				foreach(Atom a in lc.on) {
+					g.FillRectangle(br, GetCanvXPos(a.x) - sz / 2, GetCanvYPos(a.y) - sz / 2, sz, sz);
+				}
+			}
+			c.Invalidate();
+		}
+		private void Redraw() {
+			Graphics g = Graphics.FromImage(c.image);
+			g.Clear(Color.Transparent);
+			g.SmoothingMode = SmoothingMode.AntiAlias;
+			if(gs.axes) {
+				int w = splitPanel.Panel2.Width;
+				int h = splitPanel.Panel2.Height;
+				g.DrawLine(gs.axisPen, w / 2, 0, w / 2, h);
+				g.DrawLine(gs.axisPen, 0, h / 2, w, h / 2);
+			}
+			foreach(LatticeChange lc in latChanges) {
+				int sz = lat.aSize;
+				SolidBrush br = gs.fgBrush;
+				foreach(Atom a in lc.on) {
+					g.FillRectangle(br, GetCanvXPos(a.x) - sz / 2, GetCanvYPos(a.y) - sz / 2, sz, sz);
+				}
+			}
+			c.Invalidate();
+		}
+		private void Step() {
+			step++;
+			LatticeChange lc = new LatticeChange();
+			for(int r = 1; r < lat.mat.GetLength(0) - 1; r++) {
+				for(int c = 1; c < lat.mat.GetLength(1) - 1; c++) {
+					Atom a = lat.mat[r, c];
+					if(!a.excited) {
+						int numActiveNeighbors = 0;
+						int dir = 0;
+						if(lat.mat[r + 1, c].excited) {
+							numActiveNeighbors++;
+							dir = (int)direction.S;
+						}
+						if(lat.mat[r, c + 1].excited) {
+							numActiveNeighbors++;
+							dir = (int)direction.E;
+						}
+						if(lat.mat[r - 1, c].excited) {
+							numActiveNeighbors++;
+							dir = (int)direction.N;
+						}
+						if(lat.mat[r, c - 1].excited) {
+							numActiveNeighbors++;
+							dir = (int)direction.W;
+						}
+						if(numActiveNeighbors == 1) {
+							if((dir == (int)direction.N || dir == (int)direction.S) && rnd.NextDouble() < lat.Yprobability)
+								lc.AddOn(a);
+							if((dir == (int)direction.E || dir == (int)direction.W) && rnd.NextDouble() < lat.Xprobability)
+								lc.AddOn(a);
+						}
+					}
+                }
+            }
+            stepCounter.Value = step;
+            foreach (Atom a in lc.on)
+                a.excited = true;
+            latChanges.Add(lc);
+            PaintImage(true);
+        }
+        private void PlayStep(object source, System.Timers.ElapsedEventArgs e)
+        {
+            BeginInvoke(new Action(Step), null);
+        }
+        private void StepBack()
+        {
+            if (step > 1) step--;
+            stepCounter.Value = step;
+            PaintImage(false);
+        }
+        private int GetCanvXPos(int x)
+        {
+            return splitPanel.Panel2.ClientSize.Width / 2 + lat.aSize * x;
+        }
+        private int GetCanvYPos(int y)
+        {
+            return splitPanel.Panel2.ClientSize.Height / 2 - lat.aSize * y;
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            StepBack();
+        }
+        private void fwdButton_Click(object sender, EventArgs e) {
+			Step();
+		}
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (!clock.Enabled)
+            {
+                clock.Start();
+                playButton.Text = "Pause";
+                speedBar.Visible = playSpeedLabel.Visible = true;
+            }
+            else {
+                clock.Stop();
+                playButton.Text = "Play";
+                speedBar.Visible = playSpeedLabel.Visible = false;
+            }
+        }
+        private void xProbBar_Scroll(object sender, EventArgs e) {
+            lat.Xprobability = (double)(xProbBar.Value) / 100;
+			xProbValue.Value = xProbBar.Value;
+		}
+		private void yProbBar_Scroll(object sender, EventArgs e) {
+			lat.Yprobability = (double)(yProbBar.Value) / 100;
+			yProbValue.Value = yProbBar.Value;
+		}
+		private void speedBar_Scroll(object sender, EventArgs e)
+        {
+            clock.Interval = 900 - (speedBar.Value * 8);
+        }
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            step = 1;
+            clock.Stop();
+			playButton.Text = "Play";
+			speedBar.Visible = playSpeedLabel.Visible = false;
+			lat = new Lattice();
+			lat.Xprobability = (double)(xProbBar.Value) / 100;
+			lat.Yprobability = (double)(yProbBar.Value) / 100;
+			latChanges = new List<LatticeChange>();
+            foreach (Atom a in lat.mat)
+            {
+                if (a.x == 0 && a.y == 0)
+                {
+                    LatticeChange lc = new LatticeChange();
+                    lc.AddOn(a);
+                    latChanges.Add(lc);
+                    a.excited = true;
+                }
+            }
+            stepCounter.Value = 1;
+            Redraw();
+        }
+		private void xProbValue_ValueChanged(object sender, EventArgs e) {
+			xProbBar.Value = (int) xProbValue.Value;
+			lat.Xprobability = (double)(xProbBar.Value) / 100;
+		}
+		private void yProbValue_ValueChanged(object sender, EventArgs e) {
+			yProbBar.Value = (int)yProbValue.Value;
+			lat.Yprobability = (double)(yProbBar.Value) / 100;
+		}
+	}
+	public class Canvas : Panel {
+		public Bitmap image { get; set; }
+
+		public Canvas(Bitmap b) {
+			image = b;
+			AutoSize = true;
+			DoubleBuffered = true;
+		}
+		protected override void OnPaint(PaintEventArgs e) {
+			Graphics g = e.Graphics;
+			g.DrawImage(image, 0, 0);
+		}
+	}
+	public class Lattice
+    {
+        public Atom[,] mat { get; set; }
+        public double Xprobability { get; set; }
+		public double Yprobability { get; set; }
+		public int aSize { get; set; }
+
+
+        public Lattice()
+        {
+            Xprobability = Yprobability = 1;
+            aSize = 10;
+            mat = new Atom[256, 256];
+            int xsz = mat.GetLength(1);
+            int ysz = mat.GetLength(0);
+            for (int i = 0; i < xsz; i++)
+            {
+                for (int j = 0; j < ysz; j++)
+                {
+                    mat[j, i] = new Atom(i - xsz / 2, j - ysz / 2, 0);
+                }
+            }
+        }
+    }
+    public class Atom
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+        public Boolean excited { get; set; }
+
+        public Atom(int x1, int y1, int z1)
+        {
+            x = x1;
+            y = y1;
+            z = z1;
+            excited = false;
+        }
+    }
+    public class LatticeChange
+    {
+        public List<Atom> on { get; set; }
+        public List<Atom> off { get; set; }
+
+        public LatticeChange()
+        {
+            on = new List<Atom>();
+            off = new List<Atom>();
+        }
+        public void AddOn(Atom a)
+        {
+            on.Add(a);
+        }
+        public void AddOff(Atom a)
+        {
+            off.Add(a);
+        }
+    }
+    public class GraphicsSettings
+    {
+        public SolidBrush bgBrush { get; set; }
+        public SolidBrush fgBrush { get; set; }
+        public Pen axisPen { get; set; }
+        public Boolean axes { get; set; }
+
+        public GraphicsSettings()
+        {
+            bgBrush = new SolidBrush(Color.Black);
+            fgBrush = new SolidBrush(Color.PaleTurquoise);
+            axisPen = new Pen(Color.PaleTurquoise, 1);
+            axes = true;
+        }
+    }
+}
